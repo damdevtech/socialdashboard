a14bdaee7a0dd8b2386aad6643fe39a421064cd7
exceldigital1 <saheed.adedapo111@gmail.com>
2026-02-20 12:49:09 +0100

    my first commit

---
+++ src/layouts/Dashboard.jsx
@@ -0,0 +1,16 @@
+import Sidebar from "../components/sidebar";
+import TopNav from "../components/TopNav";
+import Home from "../pages/Home";
+
+function Dashboard() {
+    return (
+        <div>
+            {/* <Sidebar />
+            <TopNav /> */}
+            <Home />
+
+        </div>
+    );
+}
+
+export default Dashboard;
