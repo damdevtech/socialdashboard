ac16b5ae9115fab8a79de86c83a602a240baa462
exceldigital1 <saheed.adedapo111@gmail.com>
2026-03-03 12:31:59 +0100

    solved version issue

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,14 +1,9 @@
-import SideBar from "../components/sidebar";
-import TopNav from "../components/TopNav";
 import Home from "../pages/Home";
 
 function Dashboard() {
     return (
         <div>
-            {/* <SideBar />
-            <TopNav /> */}
             <Home />
-
         </div>
     );
 }
